94b3b7b476879f383a1085e1f2708b2e92176487
diegorodolfodossantos <dr.santos1990@gmail.com>
2022-11-26 23:24:00 -0300

    Implement circuit breaker in project.

--- order-service/src/main/java/com/br/diego/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/br/diego/orderservice/controller/OrderController.java
@@ -6,12 +6,15 @@ import com.br.diego.orderservice.model.Order;
 import com.br.diego.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("/api/order")
@@ -21,11 +24,15 @@ public class OrderController {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
 
     @PostMapping
     public String placeOrder(@RequestBody OrderDto orderDto){
-        boolean allProductsInStock = orderDto.getOrderLineItemsList().stream()
+        Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("inventory");
+        Supplier<Boolean> booleanSupplier = () -> orderDto.getOrderLineItemsList().stream()
                 .allMatch(orderLineItems -> inventoryClient.checkStock(orderLineItems.getSkuCode()));
+
+        boolean allProductsInStock = circuitBreaker.run(booleanSupplier, throwable -> handleErrorCase());
 
         if(allProductsInStock) {
             Order order = new Order();
@@ -38,5 +45,9 @@ public class OrderController {
         } else {
             return  "Order Failed. One of the products in the order is not in stock";
         }
+    }
+
+    private Boolean handleErrorCase() {
+        return false;
     }
 }
